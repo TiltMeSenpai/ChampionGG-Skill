aea0849aa816868ffadeea39ef2a4954e68cea07
Joshua Koike <jkoike2013@gmail.com>
2016-09-08 15:49:04 -0700

    Change from secrets.py to environ variables

--- skill.py
+++ skill.py
@@ -1,7 +1,7 @@
 from .. import Skill
 from tabulate import tabulate
 import requests
-import secrets
+import os
 
 class RoleSkill(Skill):
     async def parse(self, message, lane, best="best", number=5, question=None):
@@ -9,10 +9,10 @@ class RoleSkill(Skill):
         if lane == "mid": lane = "middle"
         if best in ["best", "top"]:
             url = "http://api.champion.gg/stats/role/{}/bestPerformance?api_key={}&page=1&limit={}".format(
-                lane, secrets.champion_gg, number)
+                lane, os.environ["champion_gg"], number)
         else:
             url = "http://api.champion.gg/stats/role/{}/worstPerformance?api_key={}&page=1&limit={}".format(
-                    lane, secrets.champion_gg, number)
+                    lane, os.environ["champion_gg"], number)
         print(url)
         champs = requests.get(url).json()
         data = [["#", "Name", "Win Percent", "Play Percent", "Ban Rate"]]
